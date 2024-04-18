3c025051def7daeb853ea1fd08d799c690a641b0
dafulo22 <fuentes_616@hotmail.com>
2024-04-18 11:19:18 -0500

    Adding project base
    Adding initial project structure

---
+++ src/main/java/com/co/uninorte/git/demo/GitAndGithubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.co.uninorte.git.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAndGithubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAndGithubDemoApplication.class, args);
+	}
+
+}
