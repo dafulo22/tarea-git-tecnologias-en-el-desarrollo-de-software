1943ba5e7623261f17eedcb9b1fe51fdae9a0b9d
dafulo22 <fuentes_616@hotmail.com>
2024-04-18 11:19:18 -0500

    feat(core): add project base
    Adding initial project structure

---
+++ src/test/java/com/co/uninorte/git/demo/GitAndGithubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.uninorte.git.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAndGithubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
