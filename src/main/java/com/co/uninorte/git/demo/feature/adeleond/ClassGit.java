f78786828d41e71b1af502386c864d65a08c6c3a
adeleond05 <61605923+adeleond05@users.noreply.github.com>
2024-04-19 19:52:15 -0500

    feat(): add new class ChangeFour
    * Add class
    
    * Update classGit
    
    * Add new class
    
    * Add class
    
    ---------
    
    Co-authored-by: Antony <adelahoz.extreme@gmail.com>

---
+++ src/main/java/com/co/uninorte/git/demo/feature/adeleond/ClassGit.java
@@ -0,0 +1,15 @@
+package com.co.uninorte.git.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClassGit {
+
+	private String classGit;
+	private String commitSecond;
+	private String changeTwo;
+
+}
