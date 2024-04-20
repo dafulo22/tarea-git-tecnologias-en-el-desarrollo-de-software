5f995d8d9293c48f724c43cfca2f22844d4d32cb
adeleond05 <61605923+adeleond05@users.noreply.github.com>
2024-04-19 19:52:15 -0500

    Feature/antony (#1)
    * Add class
    
    * Update classGit
    
    * Add new class
    
    * Add class
    
    ---------
    
    Co-authored-by: Antony <adelahoz.extreme@gmail.com>

---
+++ src/main/java/com/co/uninorte/git/demo/feature/adeleond/ChangeThree.java
@@ -0,0 +1,14 @@
+package com.co.uninorte.git.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClassGit {
+
+	private String changeThree;
+	private String update;
+
+}
