df16f62c98575482bca775c60d2c37dedc6311ba
Antony <adelahoz.extreme@gmail.com>
2024-04-19 19:15:13 -0500

    Add class

---
+++ src/main/java/com/co/uninorte/git/demo/feature/adeleond/ClassGit.java
@@ -0,0 +1,13 @@
+package com.co.uninorte.git.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClassGit {
+
+	private String classGit;
+
+}
